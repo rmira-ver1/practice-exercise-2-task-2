6373524c2cde299a711aec2e9f8a0ede7f01a219
Ralph Lawrence Mira <ralph.arim@gmail.com>
2023-12-22 10:26:18 +0800

    renamed and modified the file extension for text.txt

---
+++ hello-world-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is the former text1.txt renamed and changed the file extension
+
+message_function () {
+    local message="Hello, World!"
+    echo "$message"
+}
+
+message_function
+exit 0
