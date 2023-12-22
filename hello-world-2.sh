e3ad7ed471f370746bfa0da8ed0fb83d8e4178dc
Ralph Lawrence Mira <ralph.arim@gmail.com>
2023-12-22 10:28:53 +0800

    space fix

--- hello-world-2.sh
+++ hello-world-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This is the former text1.txt renamed and changed the file extension
-
+# THis one uses functions
 message_function () {
     local message="Hello, World!"
     echo "$message"
